e73f204ab0a7094156ea2c80ea85c24fb3dca1e6
Francisco Igor <franciscoi@tbadigital.com>
2024-07-19 12:18:40 -0700

    Use of static assets (images)

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,16 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1 className="example">Hello, Next.js!</h1>
+    <div className="example">
+      <h1 >Hello, Next.js!</h1>
+      <Image
+          src="/next.svg"
+          alt="NextJS Logo"
+          className="dark:invert my-2 inline-block"
+          width={150}
+          height={30}
+          priority
+        />
+    </div>
   );
 }
