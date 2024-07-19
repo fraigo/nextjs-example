8f85ad13fa0260a07f1b53a307465c5df2ab51f0
Francisco Igor <franciscoi@tbadigital.com>
2024-07-19 11:51:51 -0700

    TailwindCSS +Eslint setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
-export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <h1 className="example">Hello, Next.js!</h1>
+  );
 }
